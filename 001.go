c58b8ff315cc78a75f319747b02ebe4e4671f7cc
Nguyễn Hữu Nghĩa <nhnske@gmail.com>
2023-09-13 18:15:50 +0700

    add #1 solution

---
+++ 001.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main() {
+    sum := 0
+    n := 1000
+    for i:=0; i<n; i++ {
+        if i%3==0 || i%5==0 {
+            sum += i
+        }
+    }
+    fmt.Println(sum)
+}
