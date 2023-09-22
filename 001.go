fb7b1c72707c5c98e9af6f4a491e68b39ec9db61
Nguyễn Hữu Nghĩa <nhnske@gmail.com>
2023-09-22 14:40:23 +0700

    Fix: formating

--- 001.go
+++ 001.go
@@ -1,13 +1,14 @@
 package main
+
 import "fmt"
 
 func main() {
-    sum := 0
-    n := 1000
-    for i:=0; i<n; i++ {
-        if i%3==0 || i%5==0 {
-            sum += i
-        }
-    }
-    fmt.Println(sum)
+	sum := 0
+	n := 1000
+	for i := 0; i < n; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
 }
